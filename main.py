49478506ead3de3c356becd753993817474af048
Jimmy Hildingsson <jimmy.hildingsson@redeploy.com>
2025-02-24 08:26:27 +0100

    Finalization of the program

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from stats import count_book_words
 from stats import count_book_letters
+from stats import sorted_list
+import sys
 
 def get_book_text(file_path):
-    with open(file_path) as f:
+    try:
+      with open(file_path) as f:
         file_contents = f.read()
         return file_contents
+    except FileNotFoundError:
+            print("Usage: python3 main.py <path_to_book>")
+            sys.exit(1)
 
 def main():
-    file_path = "books/frankenstein.txt"
+    file_path = sys.argv[1]
     book_text = get_book_text(file_path)
     word_count = count_book_words(book_text)
     letter_count = count_book_letters(book_text)
-    print(letter_count)
-    print(word_count)
-
+    new_list = sorted_list(letter_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in new_list:
+        for key, value in item.items():
+            print(f"{key}: {value}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,14 +1,18 @@
 def count_book_words(book_text):
     word_count = len(book_text.split())
-    return f"{word_count} words found in the document"
+    return word_count
 
 def count_book_letters(book_text):
     book_letters = {}
     letters = book_text.lower()
     for letter in letters:
         book_letters[letter] = book_letters.get(letter, 0) + 1
-#        if book_letters.get(letter) == None:
-#            book_letters[letter] = 1
-#        else:
-#           book_letters[letter] += 1
     return book_letters
+
+def sorted_list(book_letters):
+    test_list = []
+    sorted_list = sorted(book_letters.items(), key=lambda item: item[1], reverse=True)
+    for key, value in sorted_list:
+        if key.isalpha():
+            test_list.append({key: value})
+    return test_list
